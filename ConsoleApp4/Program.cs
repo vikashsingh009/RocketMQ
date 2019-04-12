8efa13e1c760e4ce0b15e1436d24bd1cce909d54
vikash.singh@bizruntime.com <vikashs@althing.one>
2019-04-12 18:12:28 +0530

    Consumer

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,38 @@
+﻿using org.apache.rocketmq.client.consumer;
+using org.apache.rocketmq.common.consumer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string topic = "topicF";
+                DefaultMQPushConsumer _consumer = new DefaultMQPushConsumer("prod900900");
+                _consumer.setNamesrvAddr("localhost:9876");
+               // _consumer.setVipChannelEnabled(false);
+                _consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+                _consumer.subscribe(topic, "*");
+
+                RoutingListener listener = new RoutingListener();
+                _consumer.registerMessageListener(listener);
+                _consumer.start();
+
+                //_consumer.createTopic("TBW102", topic, 4);
+
+               // Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/RoutingListener.cs
@@ -0,0 +1,68 @@
+﻿using java.util;
+using org.apache.rocketmq.client.consumer.listener;
+using org.apache.rocketmq.common.message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class RoutingListener : MessageListenerConcurrently
+    {
+        public RoutingListener()
+        {
+        }
+
+
+        //public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+        //        ConsumeConcurrentlyContext context)
+        //{
+        //    Console.WriteLine("%s Receive New Messages: %s %n", msgs);
+        //    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        //}
+
+        public ConsumeConcurrentlyStatus consumeMessage(List msgs, ConsumeConcurrentlyContext context)
+
+        {
+            try
+
+            {
+                var messageList = msgs.iterator();
+                while (messageList.hasNext())
+                {
+                    MessageExt msg = (MessageExt)messageList.next();
+                    byte[] msgbody = msg.getBody();
+                    string body = Encoding.UTF8.GetString(msgbody);
+                    Console.WriteLine("===> consumed msg :" + body);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        }
+        //public ConsumeOrderlyStatus consumeMessage(List msgs, ConsumeOrderlyContext context)
+        //{
+        //    try
+
+        //    {
+        //        var messageList = msgs.iterator();
+        //        while (messageList.hasNext())
+        //        {
+        //            MessageExt msg = (MessageExt)messageList.next();
+        //            byte[] msgbody = msg.getBody();
+        //            string body = Encoding.UTF8.GetString(msgbody);
+        //            Console.WriteLine("===> consumed msg :" + body);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //    }
+        //    return ConsumeOrderlyStatus.SUCCESS;
+        //}
+    }
+}
